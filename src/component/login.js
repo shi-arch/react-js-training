7a9690aeb4ea569a23cc3fb32e86c7a2e381d8ba
Shivram Kashyap <shivram.kashyap@infoaxon.com>
2022-05-11 23:02:36 +0530

    commit

--- src/component/login.js
+++ src/component/login.js
@@ -15,15 +15,18 @@ const Login = (props) => {
     const location = useLocation();
     const [err, setErr] = useState({})
     const [registerObj, setRegisterObj] = useState({})
+    const [email, setEmail] = useState("")
     const [show, setShow] = useState(false)
     const [showVeri, setShowVeri] = useState(false)
+    const [type, setType] = useState("password")    
+    const [password, setPassword] = useState({})
 
     useEffect(() => {
         if (location && !location.state) {
             navigate("/")
         }
     }, [])
-    const validate = () => {
+    const emailValidation = () => {
         let isValid = true
         if (!registerObj.email) {
             isValid = false
@@ -31,6 +34,24 @@ const Login = (props) => {
         } else if (registerObj.email && (validateEmail(registerObj.email) == null)) {
             isValid = false
             setErr({ err: "Incorrect email formate", type: "email" })
+        }
+        return isValid
+    }
+    const forgotEmailValidation = () => {
+        let isValid = true
+        if (!email) {
+            isValid = false
+            setErr({ err: "Please enter the email", type: "email" })
+        } else if (email && (validateEmail(email) == null)) {
+            isValid = false
+            setErr({ err: "Incorrect email formate", type: "email" })
+        }
+        return isValid
+    }
+    const validate = () => {
+        let isValid = true
+        if(!emailValidation()){
+            isValid = false
         } else if (!registerObj.password) {
             isValid = false
             setErr({ err: "Please enter the password", type: "password" })
@@ -48,8 +69,8 @@ const Login = (props) => {
     }
     const handleClose = () => {
         setShow(false)
-        swal("Verification!", "verification code has sent to your email id", "success");
-        setShowVeri(true)
+        //swal("Verification!", "verification code has sent to your email id", "success");
+        //setShowVeri(true)
     }
     const changeFun = (e) => {
         const { value, name } = e.target
@@ -58,6 +79,39 @@ const Login = (props) => {
         obj[name] = value
         setRegisterObj(obj)
         //setEmail(value)
+    }
+    const setEmailStr = (e) => {
+        const { value, name } = e.target
+       setEmail(value)
+        //setEmail(value)
+    }
+    const passwordValidation = () => {
+        let isValid = true
+        const regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+        if (!password.newPass) {
+            isValid = false
+            setErr({ err: "Please enter the new password", type: "newPass" })
+        } else if (password.newPass && (!regularExpression.test(password.newPass))) {
+            debugger
+            isValid = false
+            setErr({ err: "Your password must have length 6 to 16 and must have a number and special character", type: "newPass" })
+        } else if (!password.confirmPass) {
+            isValid = false
+            setErr({ err: "Please confirm password", type: "confirmPass" })
+        } else if (password.newPass !== password.confirmPass) {
+            isValid = false
+            setErr({ err: "Password does not match", type: "confirmPass" })
+        } else {
+            isValid = true
+            setErr({ err: "", type: "" })
+        }
+        return isValid
+    }
+    const setPasswordDetails = (e) => {
+        const { value, name } = e.target
+        let obj = { ...password };
+        obj[name] = value
+        setPassword(obj)
     }
     const handleShow = () => {
         setShow(true)
@@ -73,13 +127,13 @@ const Login = (props) => {
                     // onFailure={responseGoogle}
                     cookiePolicy={'single_host_origin'}
                 /> */}
-                <FacebookLogin
+                {/* <FacebookLogin
                     appId="1022482148392327"
                     autoLoad={true}
                     fields="name,email,picture"
                     //onClick={componentClicked}
                     //callback={responseFacebook} 
-                    />
+                    /> */}
                 <label>Email:</label><InputBox name={"email"} changeFun={changeFun} type={"email"} value={registerObj.email} placeholder={"Enter your Email"} />
                 <p style={{ color: "red" }}>{err && err.type == "email" ? err.err : ""}</p>
                 <label>Password:</label><InputBox name={"password"} changeFun={changeFun} type={"password"} value={registerObj.password} placeholder={"Enter your Password"} />
@@ -97,13 +151,21 @@ const Login = (props) => {
                         <Modal.Title>Forgot Password</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <InputBox name={"forgot_pwd"} type={"text"} placeholder={"Enter your registered email id"} />
+                        <InputBox name={"email"} changeFun={setEmailStr} type={"email"} placeholder={"Enter your registered email id"} />
+                        <span style={{color: "red"}}>{err.err}</span>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleClose}>
                             Close
                         </Button>
-                        <Button variant="primary" onClick={handleClose}>
+                        <Button variant="primary" onClick={() => {
+                            const validation = forgotEmailValidation()
+                            if(validation){
+                                setShow(false)
+                                swal("Verification!", "verification code has sent to your email id", "success");
+                                setShowVeri(true)
+                            }                            
+                        }}>
                             Send verification link
                         </Button>
                     </Modal.Footer>
@@ -113,14 +175,29 @@ const Login = (props) => {
                         <Modal.Title>Forgot Password</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <InputBox style={{marginBottom: "21px"}} name={"forgot_pwd"} type={"text"} placeholder={"Enter new password"} />
-                        <InputBox name={"forgot_pwd"} type={"text"} placeholder={"Confirm password"} />
+                        <img onClick={() => {
+                            if(type == "password"){
+                                setType("text")
+                            } else {
+                                setType("password")
+                            }                                
+                        }} src="../assets/view.png" />
+                        <InputBox style={{ marginBottom: "21px" }} changeFun={setPasswordDetails} name={"newPass"} type={"password"} placeholder={"Enter new password"} />
+                        <p style={{ color: "red", marginTop: "-20px" }}>{err && err.type == "newPass" ? err.err : ""}</p>
+                        <InputBox name={"confirmPass"} type={type} changeFun={setPasswordDetails} placeholder={"Confirm password"} />
+                        <p style={{ color: "red" }}>{err && err.type == "confirmPass" ? err.err : ""}</p>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="secondary" onClick={() => setShowVeri(false)}>
+                        <Button variant="secondary" onClick={() => 
+                            setShowVeri(false)
+                        }>
                             Close
                         </Button>
-                        <Button variant="primary" onClick={() => setShowVeri(false)}>
+                        <Button variant="primary" onClick={() => {
+                            if(passwordValidation()){
+                                setShowVeri(false)
+                            }                            
+                        }}>
                             Save password
                         </Button>
                     </Modal.Footer>
